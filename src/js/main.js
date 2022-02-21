3fee4df56b24620191a04ee26b64d6041595fdff
bhhoang818 <88428487+bhhoang818@users.noreply.github.com>
2022-02-21 18:00:28 +0700

    add

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,25 @@ const startMarquee = () => {
     $('.marquee_text').marquee({
         direction: 'left',
         duration: 15000,
-        gap: 5,
+        gap: 8,
         delayBeforeStart: 0,
         duplicated: true,
         startVisible: true
     });
 }
+const focusContact = () => {
+    $('.input').each(function () {
+        $(this).on('blur', function () {
+            if ($(this).val().trim() != "") {
+                $(this).addClass('has-val');
+            }
+            else {
+                $(this).removeClass('has-val');
+            }
+        })
+    })
+}
 $(document).ready(function () {
     startMarquee();
+    focusContact();
 })
